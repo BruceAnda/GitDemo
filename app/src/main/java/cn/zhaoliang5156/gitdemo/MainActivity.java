2beaf4a61dbfa921ea735019e944824b65d892e3
zhaoliang <2668645098@qq.com>
2018-08-29 11:42:04 +0800

    切换分支

--- app/src/main/java/cn/zhaoliang5156/gitdemo/MainActivity.java
+++ app/src/main/java/cn/zhaoliang5156/gitdemo/MainActivity.java
@@ -3,6 +3,9 @@ package cn.zhaoliang5156.gitdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 开发版本
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
